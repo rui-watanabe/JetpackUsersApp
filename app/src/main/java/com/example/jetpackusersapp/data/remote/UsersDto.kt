7c199f06234d9ab36e18ee5887104bdd1e1d208a
rui-watanabe <wataru610806@gmail.com>
2023-01-22 16:02:42 +0900

    UserModel変換メソッドの追加

--- app/src/main/java/com/example/jetpackusersapp/data/remote/UsersDto.kt
+++ app/src/main/java/com/example/jetpackusersapp/data/remote/UsersDto.kt
@@ -1,10 +1,19 @@
 package com.example.jetpackusersapp.data.remote
 
 
-import com.squareup.moshi.Json
+import com.example.jetpackusersapp.domain.model.User
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class UsersDto(
     val users: List<User?>?
 )
+
+fun UsersDto.toUsers(): List<User> {
+    return users!!.map {
+        User(
+            id = it!!.id,
+            name = it.name
+        )
+    }
+}
