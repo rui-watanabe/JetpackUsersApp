0c940095752264ed6975885c1b7534f1621adc53
rui-watanabe <wataru610806@gmail.com>
2023-01-22 16:21:43 +0900

    GetUsersUseCaseの作成

---
+++ app/src/main/java/com/example/jetpackusersapp/domain/use_case/GetUsersUseCase.kt
@@ -0,0 +1,23 @@
+package com.example.jetpackusersapp.domain.use_case
+
+import com.example.jetpackusersapp.common.NetworkResponse
+import com.example.jetpackusersapp.data.remote.toUsers
+import com.example.jetpackusersapp.domain.model.User
+import com.example.jetpackusersapp.domain.repository.UserRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class GetUsersUseCase @Inject constructor(
+    private val userRepository: UserRepository
+) {
+    operator fun invoke(): Flow<NetworkResponse<List<User>>> = flow {
+        try {
+            emit(NetworkResponse.Loading<List<User>>())
+            val users = userRepository.getUsers().toUsers()
+            emit(NetworkResponse.Success<List<User>>(users))
+        } catch (error: Exception) {
+            emit(NetworkResponse.Failure<List<User>>(error.message.toString()))
+        }
+    }
+}
